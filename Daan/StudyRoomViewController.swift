58ef088bc83abcc2fc103bda9854e19266b320c1
Jelly Kuo <jellykuo1234@gmail.com>
2017-12-16 08:01:38 +0800

    Added ProgressBar in Study Room

--- Daan/StudyRoomViewController.swift
+++ Daan/StudyRoomViewController.swift
@@ -12,7 +12,9 @@ import WebKit
 class StudyRoomViewController: UIViewController,WKNavigationDelegate {
     
     var webView: WKWebView!
+    var progressView: UIProgressView!
     let url:String
+    var myContext = 0
     
     required init?(coder aDecoder: NSCoder) {
         let apiConfig = NSDictionary(contentsOfFile:Bundle.main.path(forResource: "ApiConfig", ofType: "plist")!)
@@ -22,17 +24,31 @@ class StudyRoomViewController: UIViewController,WKNavigationDelegate {
         super.init(coder: aDecoder)
     }
     
+    deinit {
+        webView.removeObserver(self, forKeyPath: "estimatedProgress")
+        progressView.removeFromSuperview()
+    }
+    
     override func loadView() {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.navigationDelegate = self
         view = webView
+        print("WKWebView created")
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.autoresizingMask = [.flexibleWidth, .flexibleTopMargin]
+        //progressView.tintColor = view.tintColor
+        navigationController?.navigationBar.addSubview(progressView)
+        let navigationBarBounds = self.navigationController?.navigationBar.bounds
+        progressView.frame = CGRect(x: 0, y: navigationBarBounds!.size.height - 2, width: navigationBarBounds!.size.width, height: 2)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: &myContext)
 
         let myURL = URL(string: url)
         let myRequest = URLRequest(url: myURL!)
@@ -46,6 +62,29 @@ class StudyRoomViewController: UIViewController,WKNavigationDelegate {
     
     @IBAction func reloadTap(_ sender: Any) {
         webView.reload()
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard let change = change else { return }
+        if context != &myContext {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+            return
+        }
+        
+        if keyPath == "estimatedProgress" {
+            if let progress = (change[NSKeyValueChangeKey.newKey] as AnyObject).floatValue {
+                progressView.progress = progress;
+            }
+            return
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        progressView.isHidden = true
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        progressView.isHidden = false
     }
     
     /*
