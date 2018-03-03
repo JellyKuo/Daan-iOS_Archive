94342c14725f88daa286d66ac1c123b59711a2fe
Jelly Kuo <jellykuo1234@gmail.com>
2018-03-03 11:16:15 +0800

    Remove old code

--- Daan/AppDelegate.swift
+++ Daan/AppDelegate.swift
@@ -27,23 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print("FCM token: \(token ?? "nil")")
         
         //Register for notification is now handled in splash
-        /*
-        if #available(iOS 10.0, *) {
-            // For iOS 10 display notification (sent via APNS)
-            UNUserNotificationCenter.current().delegate = self
-            
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-        } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
-        }
-        
-        application.registerForRemoteNotifications()
-        */
         
         return true
     }    
